c55188e23a4b11e05b5c273b560fd57f0cd222e0
Dawid Sobański <dawid.sobanski@riotters.com>
2023-01-18 12:10:59 +0100

    Component in Blade HTML

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+import "./bootstrap";
+
+import { createApp } from "vue/dist/vue.esm-bundler";
+import MyComponent from "@/components/MyComponent.vue";
+
+const app = createApp({});
+app.component("my-component", MyComponent);
+app.mount("#app");
---
+++ resources/js/components/MyComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Hello {{ name }}, Greetings from Component!
+        <button type="btn" @click="sayHello">Click me</button>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+
+const props = defineProps({
+    name: String,
+});
+
+const sayHello = () => {
+    alert("Hello!");
+};
+onMounted(() => console.log("Component was loaded..."));
+</script>
